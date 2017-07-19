97300b828a35cf8cf66f8003b242c06545d78395
vishalpallerla <vishalpallerla@hotmail.com>
2017-07-19 16:48:27 -0500

    HTTP Mocking

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,16 @@ app.get("/",function(req,res){
   res.sendFile(path.join(__dirname,'../src/index.html'));
 });
 
+app.get('/users',function(req,res){
+  res.json(
+    [
+      {"id": 1,"firstName":"Vishal","lastName":"Pallerla","email":"vpallerla@uh.edu"},
+      {"id": 2,"firstName":"Akshay","lastName":"Pallerla","email":"apallerla@uh.edu"},
+      {"id": 3,"firstName":"Rajender","lastName":"Pallerla","email":"rpallerla@uh.edu"}
+    ]
+  );
+});
+
 app.listen(port,function(err){
   if(err)
     console.log(err);
